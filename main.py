ff02c5817ebb2f0902215e132ae0ff5931fa55ea
Asier <asigonlo@gmail.com>
2015-12-18 00:36:33 +0100

    main boilerplate

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+def main():
+	pass
+
+# -------------- main --------------
+if __name__ == '__main__':
+	try:
+		main()
+	except Exception as e:
+		traceback.print_exc()
+		sys.exit(1)
